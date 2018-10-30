714f5418c219173221957f6a854ff963f198ba86
unknown <w.andrew.pedersen@gmail.com>
2018-10-30 11:15:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CharacterCard from "./components/CharacterCard";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentWillMount() {
+    fetch("https://ghibliapi.herokuapp.com/people/")
+      .then(responce => responce.json())
+      .then(responce => {
+        this.setState({
+          data: responce
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CharacterCard data={this.state.data} />
       </div>
     );
   }
---
+++ src/components/CharacterCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+
+const CharacterCard = props => {
+  return props.data.map(character => {
+    return (
+      <Card key={character.id}>
+        <Card.Content header={"Name: " + character.name} />
+        <Card.Content description={"Age: " + character.age} />
+        <Card.Content description={"Hair Color: " + character.hair_color} />
+        <Card.Content description={"Eye Color: " + character.eye_color} />
+        <Card.Content description={"Gender: " + character.gender} />
+        <Card.Content extra />
+      </Card>
+    );
+  });
+};
+
+export default CharacterCard;
